f20a1751bd08334b9f3fa80fabe619d9028ec4ab
zen <zhangyiming748@gmail.com>
2023-04-05 11:50:10 +0800

    单独slog记录器

--- resizeVideo.go
+++ resizeVideo.go
@@ -13,11 +13,11 @@ import (
 	"strings"
 )
 
-func init() {
-	logLevel := os.Getenv("LEVEL")
-	//var level slog.Level
+var mylog *slog.Logger
+
+func SetLog(level string) {
 	var opt slog.HandlerOptions
-	switch logLevel {
+	switch level {
 	case "Debug":
 		opt = slog.HandlerOptions{ // 自定义option
 			AddSource: true,
@@ -45,22 +45,24 @@ func init() {
 			AddSource: true,
 			Level:     slog.LevelDebug, // slog 默认日志级别是 info
 		}
-
 	}
-	file := "GetFileInfo.log"
+	file := "resizeVideo.log"
 	logf, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0777)
 	if err != nil {
 		panic(err)
 	}
 	//defer logf.Close() //如果不关闭可能造成内存泄露
-	logger := slog.New(opt.NewJSONHandler(io.MultiWriter(logf, os.Stdout)))
-	slog.SetDefault(logger)
+	mylog = slog.New(opt.NewJSONHandler(io.MultiWriter(logf, os.Stdout)))
+}
+func init() {
+	l := os.Getenv("LEVEL")
+	SetLog(l)
 }
 func ResizeAllVideos(root, pattern, threads string) {
 	ResizeVideos(root, pattern, threads)
 	folders := GetAllFolder.ListFolders(root)
 	for i, folder := range folders {
-		slog.Info(fmt.Sprintf("正在处理第 %d/%d 个文件夹", i+1, len(folders)))
+		mylog.Info(fmt.Sprintf("正在处理第 %d/%d 个文件夹", i+1, len(folders)))
 		ResizeVideos(folder, pattern, threads)
 	}
 }
@@ -68,17 +70,17 @@ func ResizeVideos(src, pattern, threads string) {
 	files := GetFileInfo.GetAllVideoFileInfo(src, pattern)
 	for _, file := range files {
 		if file.Width <= 1920 || file.Height <= 1920 {
-			slog.Info("跳过", slog.Any("正常尺寸的视频", file.FullPath))
+			mylog.Info("跳过", slog.String("正常尺寸的视频", file.FullPath))
 			continue
 		}
 		if file.Width > file.Height {
-			slog.Info("横屏视频", slog.Any("视频信息", file))
+			mylog.Info("横屏视频", slog.Any("视频信息", file))
 			ResizeVideo(file, threads, "1920x1080")
 		} else if file.Width < file.Height {
-			slog.Info("竖屏视频", slog.Any("视频信息", file))
+			mylog.Info("竖屏视频", slog.Any("视频信息", file))
 			ResizeVideo(file, threads, "1080x1920")
 		} else {
-			slog.Info("正方形视频", slog.Any("视频信息", file))
+			mylog.Info("正方形视频", slog.Any("视频信息", file))
 			ResizeVideo(file, threads, "1920x1920")
 		}
 	}
@@ -86,7 +88,7 @@ func ResizeVideos(src, pattern, threads string) {
 func ResizeVideo(in GetFileInfo.Info, threads string, p string) {
 	defer func() {
 		if err := recover(); err != nil {
-			slog.Warn("错误", slog.Any("文件信息", in.FullPath))
+			mylog.Warn("错误", slog.String("文件信息", in.FullPath))
 			voiceAlert.Customize("failed", voiceAlert.Samantha)
 		}
 	}()
@@ -95,8 +97,9 @@ func ResizeVideo(in GetFileInfo.Info, threads string, p string) {
 	fname := strings.Trim(in.FullName, in.ExtName)  //仅文件名
 	mp4 := strings.Join([]string{fname, "mp4"}, ".")
 	os.Mkdir(dst, 0777)
+	mylog.Debug("新建文件夹", slog.String("全名", dst))
 	out := strings.Join([]string{dst, mp4}, string(os.PathSeparator))
-	slog.Info("io", slog.Any("源文件:", in.FullPath), slog.Any("输出文件:", out))
+	mylog.Info("io", slog.String("源文件:", in.FullPath), slog.String("输出文件:", out))
 	//ffmpeg -i 1.mp4 -strict -2 -vf scale=-1:1080 4.mp4
 	// ffmpeg -threads 2 -i 4k_Saeko_Limo.mp4 -strict -2 -vf scale=-1:1080 -c:v libx265 -threads 2 1080.mp4
 	var cmd *exec.Cmd
@@ -108,17 +111,17 @@ func ResizeVideo(in GetFileInfo.Info, threads string, p string) {
 	case "1920x1920":
 		cmd = exec.Command("ffmpeg", "-threads", threads, "-i", in.FullPath, "-strict", "-2", "-vf", "scale=1920:1920", "-threads", threads, out)
 	default:
-		slog.Warn("不正常的视频源", slog.Any("视频信息", in.FullPath))
+		mylog.Warn("不正常的视频源", slog.Any("视频信息", in.FullPath))
 	}
-	slog.Info("ffmpeg", slog.Any("生成的命令", fmt.Sprintf("生成的命令是:%s\n", cmd)))
+	mylog.Info("ffmpeg", slog.String("生成的命令", fmt.Sprintf("生成的命令是:%s\n", cmd)))
 	stdout, err := cmd.StdoutPipe()
 	cmd.Stderr = cmd.Stdout
 	if err != nil {
-		slog.Warn("cmd.StdoutPipe产生错误", err)
+		mylog.Warn("cmd.StdoutPipe", slog.Any("错误", err))
 		return
 	}
 	if err = cmd.Start(); err != nil {
-		slog.Warn("cmd.Run产生的错误", err)
+		mylog.Warn("cmd.Run", slog.Any("错误", err))
 		return
 	}
 	for {
@@ -133,12 +136,12 @@ func ResizeVideo(in GetFileInfo.Info, threads string, p string) {
 		}
 	}
 	if err = cmd.Wait(); err != nil {
-		slog.Warn("错误", slog.Any("命令执行中", err))
+		mylog.Warn("命令执行中", slog.Any("错误", err))
 		return
 	}
 	if err := os.Remove(in.FullPath); err != nil {
-		slog.Warn("删除失败", slog.Any("源文件", in.FullPath))
+		mylog.Warn("删除失败", slog.String("源文件", in.FullPath))
 	} else {
-		slog.Warn("删除成功", slog.Any("源文件", in.FullPath))
+		mylog.Warn("删除成功", slog.String("源文件", in.FullPath))
 	}
 }
